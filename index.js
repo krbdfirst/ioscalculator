1462e566475b28a6cf8738ef34ac731cac9b2e29
KRBdFirst <reshybella10@gmail.com>
2022-05-24 09:25:00 +0100

    First Upload of Calc

---
+++ index.js
@@ -0,0 +1,52 @@
+let screenDisplay = 0;
+let operation;
+let lastFigure;
+let mini = document.getElementById('output')
+
+function percent() {
+}
+function num1() {
+    lastFigure = 1;
+    mini.innerText += lastFigure
+
+}
+function num2() {
+    lastFigure = 2
+    mini.innerText += lastFigure
+}
+function num3() {
+    lastFigure = 3
+    mini.innerText += lastFigure
+}
+function num4() {
+    lastFigure = 4
+    mini.innerText += lastFigure
+}
+function num5() {
+    lastFigure = 5
+    mini.innerText += lastFigure
+}
+function num6() {
+    lastFigure = 6
+    mini.innerText += lastFigure
+}
+function num7() {
+    lastFigure = 7
+    mini.innerText += lastFigure
+}
+function num8() {
+    lastFigure = 8
+    mini.innerText += lastFigure
+}
+function num9() {
+    lastFigure = 9
+    mini.innerText += lastFigure
+}
+function num0() {
+    lastFigure = 0
+    mini.innerText += lastFigure
+}
+function clear() {
+    mini.innerText -= lastFigure
+
+}
